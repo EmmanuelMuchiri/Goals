c542ba4b73b9313f1879e94c0885bb1c65dd493c
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-04 22:33:31 +0300

    modified one of my components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalComponent } from './goal/goal.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { GoalComponent } from './goal/goal.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalComponent,
     GoalDetailComponent,
+    GoalComponent,
     StrikethroughDirective,
     DateCountPipe,
     GoalFormComponent
@@ -26,4 +26,6 @@ import { FormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
